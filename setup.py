fcb6842597e1e83de8d3a316f3b2ca092eb9d444
Lin Ge Ling <lingeling@LinmatoMacBook-Air.local>
2012-12-19 12:18:44 +0800

    initial version of my new website

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='MySite',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='Green Lin',
+      author_email='lin.green@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=[
+                        'Flask',
+                        'Flask-FlatPages'
+                        ],
 #      install_requires=['Django>=1.3'],
      )
